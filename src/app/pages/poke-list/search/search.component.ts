0db093188cb76ada969fc11d849124910e16a11b
GosiaPtak <ptakma@gmail.com>
2019-12-02 22:11:38 +0100

    search parameter

--- src/app/pages/poke-list/search/search.component.ts
+++ src/app/pages/poke-list/search/search.component.ts
@@ -2,15 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+	selector: 'app-search',
+	templateUrl: './search.component.html',
+	styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  public searchPoke: FormGroup;
-  constructor(private fb: FormBuilder) {
-    this.searchPoke = this.fb.group({ pokemon: [null, Validators.required] });
-  }
+	public searchPoke: FormGroup;
+	constructor(private fb: FormBuilder) {
+		this.searchPoke = this.fb.group({ pokemon: [null, Validators.required] });
+	}
 
-  ngOnInit() {}
+	ngOnInit() {}
+	findPokemon(event) {
+		console.log(event);
+	}
 }
